b2b2f21cb209e210b1aab7d4b13311d4d470b33a
hanyaamany19 <hanyelisadi@gmail.com>
2020-03-09 23:13:59 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/intentkelasb/Activity2.kt
@@ -0,0 +1,23 @@
+package com.example.intentkelasb
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_2.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+
+        val context=this
+
+        BtnIntentUtama2.setOnClickListener {
+            val intent= Intent(context,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
